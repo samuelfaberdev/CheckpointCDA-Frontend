7f83f58a4297c74be81abcc27ad940533250ebd1
Samuel Faber <samuel.faber@live.fr>
2024-05-13 11:28:48 +0200

    Modification de la Query getCountries

--- frontend/src/graphql/getCountries.ts
+++ frontend/src/graphql/getCountries.ts
@@ -4,8 +4,13 @@ export const getCountries = gql`
   query getCountries {
     countries {
       id
+      code
       name
       emoji
+      continent {
+        id
+        name
+      }
     }
   }
 `;
