dace89904ff2c89f633a06683d83e2ffc0de19fd
Samuel Faber <samuel.faber@live.fr>
2024-05-13 09:27:59 +0200

    Ajout d'un composant Layout simple

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import { ReactNode } from "react";
+import Header from "./Header";
+
+export default function Layout({
+  title,
+  description,
+  children,
+}: {
+  title: string;
+  description: string;
+  children: ReactNode;
+}) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="favicon.ico" />
+      </Head>
+      <Header />
+      <main className="main-content">{children}</main>
+    </>
+  );
+}
