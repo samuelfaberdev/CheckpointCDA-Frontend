3fe01f89d300cf0a6927730ade86fe503b6924c7
Samuel Faber <samuel.faber@live.fr>
2024-05-13 09:54:34 +0200

    Update du composant Header avec du style Tailwind

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,9 +2,14 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="header">
-      <h1>Checkpoint : frontend</h1>
-      <Link href="/">Countries</Link>
+    <header
+      className="header bg-[#F76C6C] p-4 gap-2 flex flex-col items-center
+    "
+    >
+      <h1 className="text-3xl font-bold">Checkpoint : frontend</h1>
+      <Link href="/" className="text-2xl font-semibold">
+        Countries
+      </Link>
     </header>
   );
 }
